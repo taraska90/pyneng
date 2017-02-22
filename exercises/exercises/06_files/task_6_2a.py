1792c7e809238973b1f18cc00b94313cd8adf2ca
tarasko <tarasko@localhost.localdomain>
2017-02-22 12:44:01 +0300

    22 aebr

--- exercises/exercises/06_files/task_6_2a.py
+++ exercises/exercises/06_files/task_6_2a.py
@@ -8,5 +8,22 @@
   Скрипт не должен выводить команды, в которых содержатся слова,
   которые указаны в списке ignore.
 '''
-
+from sys import argv
+#file_name = argv[1]
 ignore = ['duplex', 'alias', 'Current configuration']
+file_name='config_sw1.txt'
+
+with open(file_name, 'r') as f:
+    for line in f:#проходим по тексту
+        line=line.rstrip().split(' ')#формируем из строки список
+        for l in line:#перебираю элементы списка line
+            for i in ignore:  #перебираю элементы списка ignore
+                if l == i:# если элементы совпадают
+                    line = ' '#то делаем строку пустой
+        line=' '.join(line)#для всего остального собираем список в строку по пробелам
+        if line.startswith('!') | line.startswith(ignore[2]):# если строка начинается с ! или Current configuration
+            don=line#обнуляю строку
+        else:   
+            print ''.join(line.split('\n'))#иначе собираю в список по какому символу?
+        line=list(line)
+       
--- exercises/exercises/06_files/task_6_3a.py
+++ exercises/exercises/06_files/task_6_3a.py
@@ -7,3 +7,16 @@
 Дополнить скрипт:
   Отсортировать вывод по номеру VLAN
 '''
+
+with open('CAM_table.txt', 'r') as mac_list:
+    for mac in mac_list:
+        mac=mac.split(' ')
+        for m in mac:
+            if m == 'aabb.cc80.7000':
+               mac_list=[mac  ]
+               #mac_str=mac_str.rstrip()     
+               print mac_str
+#if mac[5]=='aabb.cc80.7000':
+#            mac_list=''.join(mac)
+#            print mac_list
+        
